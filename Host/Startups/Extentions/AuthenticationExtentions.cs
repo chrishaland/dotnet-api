2162302b3f332ad15e8d1458e9feb3aaa0fe4379
Chris Håland <chris.haland@outlook.com>
2021-09-05 22:45:51 +0200

    Rewrite startup as partial class with extentions

---
+++ Host/Startups/Extentions/AuthenticationExtentions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository;
+
+namespace Host
+{
+    public static class AuthenticationExtentions
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+            return services.AddDbContext<Database>(options =>
+            {
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    options.UseSqlServer(connectionString);
+                }
+                else
+                {
+                    options.UseInMemoryDatabase("Database");
+                }
+            });
+        }
+    }
+}
